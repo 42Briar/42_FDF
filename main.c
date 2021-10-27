49c4bda8d3fd1bf65f5afcfe36f07b595a6c6cd3
Briar <86962846+TheBriar@users.noreply.github.com>
2021-10-27 02:06:09 +0200

    asdf

---
+++ main.c
@@ -0,0 +1,9 @@
+/*
+	parse map into 2d array
+	check for colours as well and save those somewhere, char array? list? idfk
+	turn 3d coords into 2d isometric coords
+	draw lines between those coords (bresenhams line algorithm)
+	check for colour difference and apply gradient, otherwise normal colour
+	
+
+*/
